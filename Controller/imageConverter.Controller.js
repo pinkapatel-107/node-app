529de89e0d0aab3781e74f75d7aafb15d0a25e3f
ashraf-ck <ashrafchauhan.coderkube@gmail.com>
2024-10-09 13:03:06 +0530

    changess

--- Controller/imageConverter.Controller.js
+++ Controller/imageConverter.Controller.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
 const Image = require('../Model/image.Model');
+const { generatePaths, convertImagesToSinglePDF } = require('../Utils/convertToPDF')
 
 module.exports = {
   imageToPdf: async (req, res) => {
@@ -38,31 +39,4 @@ module.exports = {
   }
 }
 
-function generatePaths(file) {
-  const imagePath = path.join(__dirname, '..', 'Uploads', file.filename); // Go up one directory and into 'Uploads'
-  const pdfPath = path.join(__dirname, '..', 'Uploads', 'pdfs', `CK_Image_to_PDF_${file.filename.split('-')[0]}.pdf`);
-  return { pdfPath, imagePath };
-}
 
-async function convertImagesToSinglePDF(imagePaths, pdfPath) {
-  return new Promise((resolve, reject) => {
-    const doc = new PDFDocument();  // Create a new instance of PDFDocument, which is used to generate a new PDF file.
-    const writeStream = fs.createWriteStream(pdfPath);// Create a writable stream to save the generated PDF to the specified path (pdfPath)
-
-    doc.pipe(writeStream);// Pipe the PDF document's output to the write stream (i.e., the file where the PDF will be saved).
-
-    imagePaths.forEach((imagePath, index) => {
-      if (index > 0) doc.addPage(); // here merge all image  in single pdf 
-      doc.image(imagePath, {
-        fit: [500, 500],
-        align: 'center',
-        valign: 'center'
-      });
-    });
-
-    doc.end();
-
-    writeStream.on('finish', resolve);
-    writeStream.on('error', reject);
-  });
-}
--- Utils/convertToPDF.js
+++ Utils/convertToPDF.js
@@ -1,33 +1,36 @@
-const PDFDocument = require('pdfkit');
 const fs = require('fs');
+const { model } = require('mongoose');
+const path = require('path');
+const PDFDocument = require('pdfkit');
 
-const convertImageToPDF = (imagePath, pdfPath) => {
+
+function generatePaths(file) {
+  const imagePath = path.join(__dirname, '..', 'Uploads', file.filename);
+  const pdfPath = path.join(__dirname, '..', 'Uploads', 'pdfs', `CK_Image_to_PDF_${file.filename.split('-')[0]}.pdf`);
+  return { pdfPath, imagePath };
+}
+
+async function convertImagesToSinglePDF(imagePaths, pdfPath) {
   return new Promise((resolve, reject) => {
-    try {
-      const doc = new PDFDocument();
-      
-      const writeStream = fs.createWriteStream(pdfPath);
-      doc.pipe(writeStream);
+    const doc = new PDFDocument();  // Create a new instance of PDFDocument, which is used to generate a new PDF file.
+    const writeStream = fs.createWriteStream(pdfPath);// Create a writable stream to save the generated PDF to the specified path (pdfPath)
 
+    doc.pipe(writeStream);// Pipe the PDF document's output to the write stream (i.e., the file where the PDF will be saved).
+
+    imagePaths.forEach((imagePath, index) => {
+      if (index > 0) doc.addPage(); // here merge all image  in single pdf 
       doc.image(imagePath, {
-        fit: [500, 400], 
+        fit: [500, 500],
         align: 'center',
         valign: 'center'
       });
+    });
 
-      doc.end();
+    doc.end();
 
-      writeStream.on('finish', () => {
-        resolve();
-      });
-
-      writeStream.on('error', (err) => {
-        reject(err);
-      });
-    } catch (error) {
-      reject(error);
-    }
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
   });
-};
+}
 
-module.exports = convertImageToPDF;
+module.exports = { generatePaths, convertImagesToSinglePDF }
